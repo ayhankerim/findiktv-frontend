c2261f2d18e7e2db8a2e3a8d1ce6060279e57f03
Kerim Ayhan <kerimayhan@gmail.com>
2023-03-08 23:08:34 +0300

    richtext component updated

--- components/sections/rich-text.js
+++ components/sections/rich-text.js
@@ -2,8 +2,8 @@ import PropTypes from "prop-types"
 
 const RichText = ({ data }) => {
   return (
-    <div
-      className="prose prose-lg container py-12"
+    <article
+      className="NewsContent container py-12"
       dangerouslySetInnerHTML={{ __html: data.content }}
     />
   )
