ed0c6d2f9129b70fca7a760cc8138becb98f4bad
Kerim Ayhan <kerimayhan@gmail.com>
2023-04-15 06:02:58 +0300

    sentry removed

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 // This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-const { withSentryConfig } = require("@sentry/nextjs")
 /**
  * @type {import('next').NextConfig}
  */
@@ -142,8 +138,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-module.exports = withSentryConfig(
-  module.exports,
-  { silent: true },
-  { hideSourcemaps: true }
-)
