b3345df0c038aa2f4f0413dc8eb990b864ed5d92
Kerim Ayhan <kerimayhan@gmail.com>
2023-12-12 22:37:52 +0300

    onesignal bugfix

--- utils/onesignal.js
+++ utils/onesignal.js
@@ -1,9 +1,11 @@
 import OneSignal from "react-onesignal"
 
 export default async function runOneSignal() {
-  await OneSignal.init({
-    appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID,
-    allowLocalhostAsSecureOrigin: true,
-  })
-  OneSignal.showSlidedownPrompt()
+  if (!OneSignal.initialized) {
+    await OneSignal.init({
+      appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID,
+      allowLocalhostAsSecureOrigin: true,
+    })
+    OneSignal.showSlidedownPrompt()
+  }
 }
