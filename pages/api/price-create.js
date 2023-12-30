b5af716d679f285511b96a6ea7ad6c1335851c4e
Kerim Ayhan <kerimayhan@gmail.com>
2023-12-30 22:38:56 +0300

    security changes

--- pages/api/price-create.js
+++ pages/api/price-create.js
@@ -1,5 +1,4 @@
 import { fetchAPI } from "utils/api"
-
 export default async function handler(req, res) {
   if (req.query.secret !== process.env.REVALIDATION_SECRET_TOKEN) {
     return res.status(401).json({ message: "Invalid token" })
