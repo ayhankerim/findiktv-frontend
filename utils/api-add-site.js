01a486f10f97b5f2350c5e82e08f2e6808916d01
Kerim Ayhan <kerimayhan@gmail.com>
2024-01-31 01:10:04 +0300

    hook for auto articles for price page added

--- utils/api-add-site.js
+++ utils/api-add-site.js
@@ -1,7 +1,3 @@
-import qs from "qs"
-import Moment from "moment"
-import "moment/locale/tr"
-
 export function getStrapiURL(path) {
   return `${
     process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337"
